064b1bd65548e3802ba75fdc76e7907ae37563d5
Nisarg Bhatt <nisargrbhatt@gmail.com>
2026-03-27 22:25:13 +0530

    Community tree page added

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
@@ -10,16 +9,15 @@ const config = defineConfig({
 	plugins: [
 		devtools(),
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
 	],
 	server: {
 		allowedHosts: [".trycloudflare.com"],
+	},
+	resolve: {
+		tsconfigPaths: true,
 	},
 });
 
